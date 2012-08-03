0cb992af2f2a2756a15c33cacb48c98b243f0d29
Defenestrayte <Dpb2006@gmail.com>
2012-08-03 13:25:31 +1200

    Added Parsing base files

---
+++ LabAssign/src/dataParsing/StudentSelections.java
@@ -0,0 +1,21 @@
+package dataParsing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentSelections {
+
+	
+	List<TimeslotParser> firstChoice = new ArrayList<TimeslotParser>();
+	List<TimeslotParser> secondChoice = new ArrayList<TimeslotParser>();
+	List<TimeslotParser> thirdChoice = new ArrayList<TimeslotParser>();
+	
+	int choicesCount;
+	
+	
+	
+	
+	
+
+
+}
