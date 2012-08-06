82718c395d61268a50b11526374dd9dd956a412c
boothanto <boothanto@kipp-cafe.ecs.vuw.ac.nz>
2012-08-06 13:26:32 +1200

    Added constructor for students array in NaiveSortTest

--- LabAssign/src/testing/NaiveSortTest.java
+++ LabAssign/src/testing/NaiveSortTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import algorithmDataStructures.Student;
 import algorithmDataStructures.Timeslot;
+import algorithms.NaiveSort;
 
 public class NaiveSortTest {
 
@@ -15,7 +16,13 @@ public class NaiveSortTest {
 	//Create list of Student objects
 	//Create list of Lab objects
 	//Create new NaiveSort
-		
+		String first = "a";
+		String last = "z";
+		for(int i=0; i<10; i++){
+			students.add(new Student(i, first, last, 007));
+			first.concat(last);
+			last.concat(first);
+		}
 		new NaiveSort(labs, new ArrayList<Timeslot>(), students);
 
 	}
