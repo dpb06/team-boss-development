e7a247133aa77c28939f9dfe2df4b640e5cdbb8d
haydnnewport <haydnnewport@gmail.com>
2012-09-24 16:34:42 +1200

    Added basic case test to BossSort test suite

--- LabAssign/src/algorithmDataStructures/AlgorithmOutput.java
+++ LabAssign/src/algorithmDataStructures/AlgorithmOutput.java
@@ -6,4 +6,12 @@ import java.util.HashMap;
 @SuppressWarnings("serial")
 public class AlgorithmOutput extends HashMap<Timeslot,ArrayList<Student>>{
 
+	private ArrayList<Student> flagged = new ArrayList<Student>();
+	
+	public void addFlagged(Student s){
+		flagged.add(s);
+	}
+	public ArrayList<Student> getFlagged(){
+		return flagged;
+	}
 }
